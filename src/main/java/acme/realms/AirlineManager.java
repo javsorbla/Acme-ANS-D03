bf2d2dd3d4ea1e753664c92425c703320b02748f
HGD6373 <javsorbla@alum.us.es>
2025-02-25 00:13:00 +0100

    Add AirlineManager role

---
+++ src/main/java/acme/realms/AirlineManager.java
@@ -0,0 +1,53 @@
+
+package acme.realms;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import acme.client.components.basis.AbstractRole;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidString;
+import acme.client.components.validation.ValidUrl;
+
+public class AirlineManager extends AbstractRole {
+
+	// Serialisation version --------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------
+
+	@Mandatory
+	@ValidString(pattern = "^[A-Z]{2-3}\\d{6}$")
+	@Column(unique = true)
+	private String				identifier;
+
+	@Mandatory
+	@ValidNumber(min = 0)
+	@Automapped
+	private int					airlineExperience;
+
+	@Mandatory
+	@ValidMoment(past = true)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				dateOfBirth;
+
+	@Optional
+	@ValidUrl
+	@Automapped
+	private String				picture;
+
+	// Relationships -----------------------------------------------------
+
+	// @Mandatory
+	// @Valid
+	// @ManyToOne(optional = false)?
+	// Airline airline	
+}

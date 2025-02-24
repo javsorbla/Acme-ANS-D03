98c2d0a5b3655c47c20adcff0bdb0b44923c9611
javcasrod1 <javcasrod1@alum.us.es>
2025-02-24 23:45:12 +0100

    Creation of flight crew member entity - S3

---
+++ src/main/java/acme/realms/flightcrewmember/FlightCrewMember.java
@@ -0,0 +1,74 @@
+
+package acme.realms.flightcrewmember;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractRole;
+import acme.client.components.datatypes.Money;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoney;
+import acme.client.components.validation.ValidString;
+import acme.entities.airline.Airline;
+import acme.validation.ValidLongText;
+import acme.validation.ValidPhone;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class FlightCrewMember extends AbstractRole {
+
+	// Serialisation version --------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Mandatory Attributes -------------------------------------------------------------
+
+	@Mandatory
+	@ValidString
+	@Column(unique = true)
+	private String				employeeCode;
+
+	@Mandatory
+	@ValidPhone
+	@Automapped
+	private String				phoneNumber;
+
+	@Mandatory
+	@ValidLongText
+	@Automapped
+	private String				languageSkills;
+
+	@Mandatory
+	@Valid
+	@Automapped
+	private AvailabilityStatus	availabilityStatus;
+
+	@Mandatory
+	@ValidMoney
+	@Automapped
+	private Money				salary;
+
+	// @Optional Attributes -------------------------------------------------------------
+
+	@Optional
+	@Valid
+	@Automapped
+	private Integer				experienceYears;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	@Automapped
+	private Airline				airline;
+
+}

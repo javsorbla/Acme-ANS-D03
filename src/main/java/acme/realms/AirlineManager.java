fdfa0e4d10b977787a6c59714cd2c1cc9eab52a5
HGD6373 <javsorbla@alum.us.es>
2025-02-25 00:41:38 +0100

    Add needed annotations to AirlineManager

--- src/main/java/acme/realms/AirlineManager.java
+++ src/main/java/acme/realms/AirlineManager.java
@@ -4,6 +4,7 @@ package acme.realms;
 import java.util.Date;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
@@ -15,7 +16,12 @@ import acme.client.components.validation.ValidMoment;
 import acme.client.components.validation.ValidNumber;
 import acme.client.components.validation.ValidString;
 import acme.client.components.validation.ValidUrl;
+import lombok.Getter;
+import lombok.Setter;
 
+@Entity
+@Getter
+@Setter
 public class AirlineManager extends AbstractRole {
 
 	// Serialisation version --------------------------------------------

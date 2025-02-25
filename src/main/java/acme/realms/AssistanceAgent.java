a7bff2ec1e62f67559bef1805e454f02a0e1f2fb
alereyper <alereyper@alum.us.es>
2025-02-25 10:53:52 +0100

    Added Assistance agent entity

---
+++ src/main/java/acme/realms/AssistanceAgent.java
@@ -0,0 +1,21 @@
+
+package acme.realms;
+
+import javax.persistence.Entity;
+
+import acme.client.components.basis.AbstractRole;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class AssistanceAgent extends AbstractRole {
+
+	// Serialisation version --------------------------------------------------
+
+	private static final long serialVersionUID = 1L;
+
+	// Attributes -------------------------------------------------------------
+
+}

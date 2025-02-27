4f6e84c1cc341e0a74b5c5f02d8e14276db37b9f
alereyper <alereyper@alum.us.es>
2025-02-27 17:41:52 +0100

    Added first version of AssistanceAgent realm

--- src/main/java/acme/realms/AssistanceAgent.java
+++ src/main/java/acme/realms/AssistanceAgent.java
@@ -1,9 +1,24 @@
 
 package acme.realms;
 
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractRole;
+import acme.client.components.datatypes.Money;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidMoney;
+import acme.client.components.validation.ValidString;
+import acme.client.components.validation.ValidUrl;
+import acme.entities.airline.Airline;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,8 +29,48 @@ public class AssistanceAgent extends AbstractRole {
 
 	// Serialisation version --------------------------------------------------
 
-	private static final long serialVersionUID = 1L;
+	private static final long	serialVersionUID	= 1L;
 
 	// Attributes -------------------------------------------------------------
+
+	@Mandatory
+	@ValidString(pattern = "^[A-Z]{2-3}\\d{6}$")
+	@Column(unique = true)
+	private String				employeeCode;
+
+	@Mandatory
+	@ValidString(max = 255)
+	@Automapped
+	private String				languages;
+
+	@Mandatory
+	@ValidMoment(past = true)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				startDate;
+
+	@Optional
+	@ValidString(max = 255)
+	@Automapped
+	private String				bio;
+
+	@Optional
+	@ValidMoney
+	@Automapped
+	private Money				salary;
+
+	@Optional
+	@ValidUrl
+	@Automapped
+	private String				photo;
+
+	// Derived attributes -------------------------------------------------------------
+
+	// Relationships ------------------------------------------------------------------
+
+	//LA RELACION PUEDE NO SER ASI
+	@Mandatory
+	@Valid
+	//@OneToOne
+	private Airline				airline;
 
 }

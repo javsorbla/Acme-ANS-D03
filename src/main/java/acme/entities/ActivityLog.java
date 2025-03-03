5145f103a09ee73419d0a080edf480fea1a3b768
javcasrod1 <javcasrod1@alum.us.es>
2025-03-03 23:00:18 +0100

    Creation of activity log entity - S3

---
+++ src/main/java/acme/entities/ActivityLog.java
@@ -0,0 +1,54 @@
+
+package acme.entities;
+
+import java.util.Date;
+
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidString;
+import acme.entities.leg.Leg;
+
+public class ActivityLog extends AbstractEntity {
+
+	// Serialisation version --------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Mandatory
+	@ValidMoment(past = true)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				registrationMoment;
+
+	@Mandatory
+	@ValidString(max = 50)
+	@Automapped
+	private String				incidentType;
+
+	@Mandatory
+	@ValidString(max = 255)
+	@Automapped
+	private String				description;
+
+	@Mandatory
+	@ValidNumber(min = 0, max = 10)
+	@Automapped
+	private Integer				severityLevel;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+	@Mandatory
+	@Valid
+	@ManyToOne
+	private Leg					activityLogLeg;
+}

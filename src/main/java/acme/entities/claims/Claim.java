4be25ad101d26aff1e484ef2ec9991e03b4b861b
alereyper <alereyper@alum.us.es>
2025-03-08 15:11:55 +0100

    Fixed some constrains and some relations

--- src/main/java/acme/entities/claims/Claim.java
+++ src/main/java/acme/entities/claims/Claim.java
@@ -3,6 +3,7 @@ package acme.entities.claims;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.validation.Valid;
 
@@ -13,7 +14,12 @@ import acme.client.components.validation.ValidEmail;
 import acme.client.components.validation.ValidMoment;
 import acme.client.components.validation.ValidString;
 import acme.realms.AssistanceAgent;
+import lombok.Getter;
+import lombok.Setter;
 
+@Entity
+@Getter
+@Setter
 public class Claim extends AbstractEntity {
 
 	// Serialisation version --------------------------------------------------
@@ -43,12 +49,12 @@ public class Claim extends AbstractEntity {
 	private ClaimType			type;
 
 	@Mandatory
-	//@Valid
+	@Valid
 	@Automapped
 	private boolean				indicator;
 
 	@Mandatory
-	//@Valid
+	@Valid
 	@Automapped
 	private boolean				draftMode;
 	// Derived attributes -----------------------------------------------------

ee6301c7a6d526a563c4b216f357180b7b99a42f
Nuno José del Pino Escalante <nundelesc@alum.us.es>
2025-03-09 16:10:25 +0100

    Validators and Involves entity added to the project

--- src/main/java/acme/realms/Technician.java
+++ src/main/java/acme/realms/Technician.java
@@ -8,6 +8,7 @@ import acme.client.components.basis.AbstractRole;
 import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidNumber;
 import acme.client.components.validation.ValidString;
 import lombok.Getter;
 import lombok.Setter;
@@ -43,6 +44,7 @@ public class Technician extends AbstractRole {
 	private boolean				anualHealthTest;
 
 	@Mandatory
+	@ValidNumber(min = 0)
 	@Automapped
 	private Integer				experienceYears;
 

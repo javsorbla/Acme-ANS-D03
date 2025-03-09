579dc33699a25b0ebed23c3e7f8d17d6e234b166
HGD6373 <javsorbla@alum.us.es>
2025-03-09 19:47:02 +0100

    Add incomplete version of flight

--- src/main/java/acme/entities/flight/Flight.java
+++ src/main/java/acme/entities/flight/Flight.java
@@ -1,6 +1,9 @@
 
 package acme.entities.flight;
 
+import javax.persistence.Entity;
+import javax.persistence.Transient;
+
 import acme.client.components.basis.AbstractEntity;
 import acme.client.components.datatypes.Money;
 import acme.client.components.mappings.Automapped;
@@ -8,7 +11,14 @@ import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidMoney;
 import acme.client.components.validation.ValidString;
+import acme.client.helpers.SpringHelper;
+import acme.entities.leg.Leg;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@Entity
 public class Flight extends AbstractEntity {
 
 	// Serialisation version --------------------------------------------
@@ -20,29 +30,40 @@ public class Flight extends AbstractEntity {
 	@Mandatory
 	@ValidString(min = 1, max = 50)
 	@Automapped // unico?
-	String						tag;
+	private String				tag;
 
 	@Mandatory
 	//@Valid
 	@Automapped
-	boolean						selfTransfer;
+	private boolean				selfTransfer;
 
 	@Mandatory
 	@ValidMoney //By default -> (min = 0.00, max = 1000000.00)
 	@Automapped
-	Money						cost;
+	private Money				cost;
 
 	@Optional
 	@ValidString
 	@Automapped
-	String						description;
+	private String				description;
 
-	//@Mandatory
+	@Mandatory
 	//@Valid
-	//@Automapped
-	//private boolean				publish; // Attribute needed for future deliverables
+	@Automapped
+	private boolean				publish; // Attribute needed for future deliverables
 
 	//Derived attributes-------------------------------------------------
+
+
+	@Transient //Necesario?
+	private Leg getFirstLeg() {
+		Leg firstLeg;
+		FlightRepository flightRepository;
+
+		flightRepository = SpringHelper.getBean(FlightRepository.class);
+		firstLeg = flightRepository.getFlightFirstLeg(this.getId());
+		return firstLeg;
+	}
 
 	// Relationships -----------------------------------------------------
 
--- src/main/java/acme/entities/flight/FlightRepository.java
+++ src/main/java/acme/entities/flight/FlightRepository.java
@@ -1,6 +1,7 @@
 
 package acme.entities.flight;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import acme.client.repositories.AbstractRepository;
@@ -9,6 +10,7 @@ import acme.entities.leg.Leg;
 @Repository
 public interface FlightRepository extends AbstractRepository {
 
-	Leg getFlightFirtLeg(int flightId);
+	@Query("select l from Leg l where l.flight.id = :flightId and l.departure = (select min(leg.departure) from Leg leg where leg.flight.id = :flightId)")
+	Leg getFlightFirstLeg(int flightId);
 
 }

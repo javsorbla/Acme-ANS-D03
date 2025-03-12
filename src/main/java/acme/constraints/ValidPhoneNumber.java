e2d10bd35af7670632b343798028f72d2cd1cd62
HGD6373 <javsorbla@alum.us.es>
2025-03-12 03:00:23 +0100

    Minor bug fix

--- src/main/java/acme/constraints/ValidPhoneNumber.java
+++ src/main/java/acme/constraints/ValidPhoneNumber.java
@@ -9,7 +9,6 @@ import java.lang.annotation.Target;
 import javax.validation.Constraint;
 import javax.validation.Payload;
 import javax.validation.ReportAsSingleViolation;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
 @Target(ElementType.FIELD)
@@ -17,7 +16,6 @@ import javax.validation.constraints.Pattern;
 @Constraint(validatedBy = {})
 @ReportAsSingleViolation
 
-@NotBlank
 @Pattern(regexp = "^\\+?\\d{6,15}$")
 public @interface ValidPhoneNumber {
 

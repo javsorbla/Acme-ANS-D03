fdfbd0bd395acc3699da31d8db460f9c837dd878
Nuno José del Pino Escalante <nundelesc@alum.us.es>
2025-03-12 11:32:47 +0100

    Entity fix

--- src/main/java/acme/entities/maintenanceRecord/MaintenanceRecord.java
+++ src/main/java/acme/entities/maintenanceRecord/MaintenanceRecord.java
@@ -10,11 +10,12 @@ import javax.persistence.TemporalType;
 import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractEntity;
+import acme.client.components.datatypes.Money;
 import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidMoment;
-import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidMoney;
 import acme.client.components.validation.ValidString;
 import acme.entities.aircraft.Aircraft;
 import acme.realms.Technician;
@@ -32,7 +33,7 @@ public class MaintenanceRecord extends AbstractEntity {
 	// Attributes -------------------------------------------------------------
 
 	@Mandatory
-	@ValidMoment
+	@ValidMoment(min = "2000/01/01 00:00:00") // No hay máximo
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date					moment;
 
@@ -47,9 +48,9 @@ public class MaintenanceRecord extends AbstractEntity {
 	private Date					nextInspectionDate;
 
 	@Mandatory
-	@ValidNumber(min = 0)
+	@ValidMoney //By default -> (min = 0.00, max = 1000000.00)
 	@Automapped
-	private Integer					estimatedCost;
+	private Money					estimatedCost;
 
 	// @Optional Attributes -------------------------------------------------------------
 

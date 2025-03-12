425fa6c9ad88954ca364e92bd5d2ab35a12f7447
HGD6373 <javsorbla@alum.us.es>
2025-03-12 03:02:41 +0100

    Not blank restriction added to iata code

--- src/main/java/acme/constraints/ValidIataCode.java
+++ src/main/java/acme/constraints/ValidIataCode.java
@@ -9,6 +9,7 @@ import java.lang.annotation.Target;
 import javax.validation.Constraint;
 import javax.validation.Payload;
 import javax.validation.ReportAsSingleViolation;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
 @Target(ElementType.FIELD)
@@ -16,6 +17,7 @@ import javax.validation.constraints.Pattern;
 @Constraint(validatedBy = {})
 @ReportAsSingleViolation
 
+@NotBlank
 @Pattern(regexp = "^[A-Z]{3}$")
 public @interface ValidIataCode {
 

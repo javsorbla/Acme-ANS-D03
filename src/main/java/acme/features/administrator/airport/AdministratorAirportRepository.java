de2ef8d53d1bbf1ce4ebaf79c736c5e53ae68a9c
alereyper <alereyper@alum.us.es>
2025-03-16 20:50:27 +0100

    Added airports repository

---
+++ src/main/java/acme/features/administrator/airport/AdministratorAirportRepository.java
@@ -0,0 +1,18 @@
+
+package acme.features.administrator.airport;
+
+import java.util.Collection;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import acme.client.repositories.AbstractRepository;
+import acme.entities.airport.Airport;
+
+@Repository
+public interface AdministratorAirportRepository extends AbstractRepository {
+
+	@Query("SELECT a FROM Airport a")
+	Collection<Airport> findAllAirports();
+
+}

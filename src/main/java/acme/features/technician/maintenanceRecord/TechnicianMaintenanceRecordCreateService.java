7d327e694c359c326e3b51f6c0d345a35bd58d65
Nuno José del Pino Escalante <nundelesc@alum.us.es>
2025-03-24 13:18:31 +0100

    Create Maintenance Record started

--- src/main/java/acme/features/technician/maintenanceRecord/TechnicianMaintenanceRecordCreateService.java
+++ src/main/java/acme/features/technician/maintenanceRecord/TechnicianMaintenanceRecordCreateService.java
@@ -1,6 +1,8 @@
 
 package acme.features.technician.maintenanceRecord;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import acme.client.services.AbstractGuiService;
 import acme.client.services.GuiService;
 import acme.entities.maintenanceRecord.MaintenanceRecord;
@@ -8,5 +10,49 @@ import acme.realms.technician.Technician;
 
 @GuiService
 public class TechnicianMaintenanceRecordCreateService extends AbstractGuiService<Technician, MaintenanceRecord> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	private TechnicianMaintenanceRecordRepository repository;
+
+
+	// AbstractGuiService interface -------------------------------------------
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		MaintenanceRecord maintenanceRecord;
+		maintenanceRecord = new MaintenanceRecord();
+		super.getBuffer().addData(maintenanceRecord);
+	}
+
+	@Override
+	public void bind(final MaintenanceRecord maintenanceRecord) {
+		Technician technician = (Technician) super.getRequest().getPrincipal().getActiveRealm();
+		maintenanceRecord.setTechnician(technician);
+		assert maintenanceRecord != null;
+		super.bindObject(maintenanceRecord, "moment", "status", "nextInspectionDate", "estimatedCost", "notes", "aircraft");
+		maintenanceRecord.setTechnician(technician);
+	}
+
+	@Override
+	public void validate(final MaintenanceRecord maintenanceRecord) {
+
+	}
+
+	@Override
+	public void perform(final MaintenanceRecord maintenanceRecord) {
+		assert maintenanceRecord != null;
+
+		this.repository.save(maintenanceRecord);
+	}
+
+	@Override
+	public void unbind(final MaintenanceRecord maintenanceRecord) {
+	}
 
 }

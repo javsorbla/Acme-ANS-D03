08cab2669571874bf7e3ec58501ccff0b122ec51
alereyper <alereyper@alum.us.es>
2025-03-27 16:18:14 +0100

    Minor bugs fixed

--- src/main/java/acme/features/assistanceagent/claim/AssistanceAgentClaimCreateService.java
+++ src/main/java/acme/features/assistanceagent/claim/AssistanceAgentClaimCreateService.java
@@ -2,9 +2,11 @@
 package acme.features.assistanceagent.claim;
 
 import acme.client.services.AbstractGuiService;
+import acme.client.services.GuiService;
 import acme.entities.claims.Claim;
 import acme.realms.assistanceagent.AssistanceAgent;
 
+@GuiService
 public class AssistanceAgentClaimCreateService extends AbstractGuiService<AssistanceAgent, Claim> {
 
 }

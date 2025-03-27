f9bb9d585d1d0838e14281201f7b1f38e7b2061a
HGD6373 <javsorbla@alum.us.es>
2025-03-27 21:49:28 +0100

    Change csv comments

--- src/main/java/acme/features/manager/flight/AirlineManagerFlightCreateService.java
+++ src/main/java/acme/features/manager/flight/AirlineManagerFlightCreateService.java
@@ -1,6 +1,9 @@
 
 package acme.features.manager.flight;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.client.components.models.Dataset;
 import acme.client.services.AbstractGuiService;
 import acme.client.services.GuiService;
 import acme.entities.flight.Flight;
@@ -8,5 +11,56 @@ import acme.realms.manager.AirlineManager;
 
 @GuiService
 public class AirlineManagerFlightCreateService extends AbstractGuiService<AirlineManager, Flight> {
+	// Internal state ---------------------------------------------------------
 
+	@Autowired
+	private AirlineManagerFlightRepository repository;
+
+
+	// AbstractGuiService interface -------------------------------------------
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		Flight flight;
+		AirlineManager manager = (AirlineManager) super.getRequest().getPrincipal().getActiveRealm();
+
+		flight = new Flight();
+		flight.setManager(manager);
+		super.getBuffer().addData(flight);
+	}
+
+	@Override
+	public void bind(final Flight flight) {
+		super.bindObject(flight, "tag", "requiresSelfTransfer", "cost", "description");
+	}
+
+	@Override
+	public void validate(final Flight flight) {
+
+		if (!this.getBuffer().getErrors().hasErrors("tag"))
+			super.state(flight.getTag() != null, "tag", "airline-manager.flight.form.error.noTag", flight);
+		//check for errors in the other attributes and add i18n
+	}
+
+	@Override
+	public void perform(final Flight flight) {
+		assert flight != null;
+
+		this.repository.save(flight);
+	}
+
+	@Override
+	public void unbind(final Flight flight) {
+		Dataset dataset;
+
+		dataset = super.unbindObject(flight, "tag", "requiresSelfTransfer", "cost", "description");
+
+		//put values from derived attributes in dataset?
+
+		super.getResponse().addData(dataset);
+	}
 }

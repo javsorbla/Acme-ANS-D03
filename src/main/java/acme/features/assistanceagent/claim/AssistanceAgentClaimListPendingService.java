9c9db3ff4b491735173ffd10612304b4e22b4b98
alereyper <alereyper@alum.us.es>
2025-03-30 20:45:33 +0200

    Added query for pending claims

---
+++ src/main/java/acme/features/assistanceagent/claim/AssistanceAgentClaimListPendingService.java
@@ -0,0 +1,54 @@
+
+package acme.features.assistanceagent.claim;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.client.components.models.Dataset;
+import acme.client.services.AbstractGuiService;
+import acme.client.services.GuiService;
+import acme.entities.claims.Claim;
+import acme.realms.assistanceagent.AssistanceAgent;
+
+@GuiService
+public class AssistanceAgentClaimListPendingService extends AbstractGuiService<AssistanceAgent, Claim> {
+
+	//Internal state ---------------------------------------------
+
+	@Autowired
+	private AssistanceAgentClaimRepository repository;
+
+	//AbstractGuiService interface -------------------------------
+
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		Collection<Claim> claims;
+		int agentId;
+
+		agentId = super.getRequest().getPrincipal().getActiveRealm().getId();
+		claims = this.repository.findAllPendingClaimsByAgentId(agentId);
+
+		super.getBuffer().addData(claims);
+	}
+
+	@Override
+	public void unbind(final Claim claim) {
+		Dataset dataset;
+		//ClaimIndicator indicator;
+
+		//	indicator = claim.getIndicator();
+		dataset = super.unbindObject(claim, "registrationMoment", "passengerEmail", "description", "type", "publish");
+		//	dataset.put("indicator", indicator);
+		super.addPayload(dataset, claim, "registrationMoment");
+
+		super.getResponse().addData(dataset);
+	}
+
+}

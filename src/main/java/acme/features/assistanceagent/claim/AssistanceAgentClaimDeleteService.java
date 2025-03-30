bddc2c44f764a244173cb2277fb74c23e39374bd
alereyper <alereyper@alum.us.es>
2025-03-30 22:21:16 +0200

    Added first version of claims delete service

--- src/main/java/acme/features/assistanceagent/claim/AssistanceAgentClaimDeleteService.java
+++ src/main/java/acme/features/assistanceagent/claim/AssistanceAgentClaimDeleteService.java
@@ -1,11 +1,18 @@
 
 package acme.features.assistanceagent.claim;
 
+import java.util.Collection;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
+import acme.client.components.models.Dataset;
+import acme.client.components.views.SelectChoices;
 import acme.client.services.AbstractGuiService;
 import acme.client.services.GuiService;
 import acme.entities.claims.Claim;
+import acme.entities.claims.ClaimType;
+import acme.entities.leg.Leg;
+import acme.entities.trackingLogs.TrackingLog;
 import acme.realms.assistanceagent.AssistanceAgent;
 
 @GuiService
@@ -18,4 +25,68 @@ public class AssistanceAgentClaimDeleteService extends AbstractGuiService<Assist
 
 	//AbstractGuiService interface -------------------------------
 
+
+	@Override
+	public void authorise() {
+		boolean status;
+		Claim claim;
+		int id;
+		AssistanceAgent assistanceAgent;
+
+		id = super.getRequest().getData("id", int.class);
+		claim = this.repository.findClaimById(id);
+		assistanceAgent = claim == null ? null : claim.getAssistanceAgent();
+		status = super.getRequest().getPrincipal().hasRealm(assistanceAgent) && (claim == null || claim.getPublish() == false);
+		super.getResponse().setAuthorised(status);
+
+	}
+
+	@Override
+	public void load() {
+		Claim claim;
+		int id;
+
+		id = super.getRequest().getData("id", int.class);
+		claim = this.repository.findClaimById(id);
+
+		super.getBuffer().addData(claim);
+	}
+
+	@Override
+	public void bind(final Claim claim) {
+		super.bindObject(claim, "registrationMoment", "passengerEmail", "description", "type", "leg");
+	}
+
+	@Override
+	public void validate(final Claim claim) {
+
+	}
+
+	@Override
+	public void perform(final Claim claim) {
+		Collection<TrackingLog> trackingLogs;
+
+		trackingLogs = this.repository.findTrackingLogsByClaimId(claim.getId());
+		this.repository.deleteAll(trackingLogs);
+		this.repository.delete(claim);
+	}
+
+	@Override
+	public void unbind(final Claim claim) {
+		Collection<Leg> legs;
+		SelectChoices choices;
+		SelectChoices choices2;
+		Dataset dataset;
+
+		choices = SelectChoices.from(ClaimType.class, claim.getType());
+		legs = this.repository.findAllLeg();
+		choices2 = SelectChoices.from(legs, "flightNumber", claim.getLeg());
+
+		dataset = super.unbindObject(claim, "registrationMoment", "passengerEmail", "description", "type", "draftMode");
+		dataset.put("types", choices);
+		dataset.put("leg", choices2.getSelected().getKey());
+		dataset.put("legs", choices2);
+
+		super.getResponse().addData(dataset);
+	}
 }

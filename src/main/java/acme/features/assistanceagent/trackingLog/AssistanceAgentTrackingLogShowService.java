850c3d2ba24d52650429f9a88a62da0ea53c8550
alereyper <alereyper@alum.us.es>
2025-04-02 19:59:16 +0200

    Tracking logs empty services added

---
+++ src/main/java/acme/features/assistanceagent/trackingLog/AssistanceAgentTrackingLogShowService.java
@@ -0,0 +1,12 @@
+
+package acme.features.assistanceagent.trackingLog;
+
+import acme.client.services.AbstractGuiService;
+import acme.client.services.GuiService;
+import acme.entities.trackingLogs.TrackingLog;
+import acme.realms.assistanceagent.AssistanceAgent;
+
+@GuiService
+public class AssistanceAgentTrackingLogShowService extends AbstractGuiService<AssistanceAgent, TrackingLog> {
+
+}

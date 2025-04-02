a8d363435a9f7e2bd9fbb476ea7e0d687269a856
alereyper <alereyper@alum.us.es>
2025-04-02 21:21:32 +0200

    Added tracking logs show service, repository updated

--- src/main/java/acme/features/assistanceagent/trackingLog/AssistanceAgentTrackingLogShowService.java
+++ src/main/java/acme/features/assistanceagent/trackingLog/AssistanceAgentTrackingLogShowService.java
@@ -1,12 +1,67 @@
 
 package acme.features.assistanceagent.trackingLog;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.client.components.models.Dataset;
+import acme.client.components.views.SelectChoices;
 import acme.client.services.AbstractGuiService;
 import acme.client.services.GuiService;
 import acme.entities.trackingLogs.TrackingLog;
+import acme.entities.trackingLogs.TrackingLogStatus;
 import acme.realms.assistanceagent.AssistanceAgent;
 
 @GuiService
 public class AssistanceAgentTrackingLogShowService extends AbstractGuiService<AssistanceAgent, TrackingLog> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	private AssistanceAgentTrackingLogRepository repository;
+
+	// AbstractGuiService interface -------------------------------------------
+
+
+	@Override
+	public void authorise() {
+		TrackingLog trackingLog;
+		int trackingLogId;
+		int agentId;
+		boolean status;
+
+		trackingLogId = super.getRequest().getData("id", int.class);
+		trackingLog = this.repository.findTrackingLogById(trackingLogId);
+		agentId = super.getRequest().getPrincipal().getActiveRealm().getId();
+		status = trackingLog != null && trackingLog.getClaim().getAssistanceAgent().getId() == agentId;
+
+		super.getResponse().setAuthorised(status);
+
+	}
+
+	@Override
+	public void load() {
+		TrackingLog trackingLog;
+		int trackingLogId;
+
+		trackingLogId = super.getRequest().getData("id", int.class);
+		trackingLog = this.repository.findTrackingLogById(trackingLogId);
+
+		super.getBuffer().addData(trackingLog);
+
+	}
+
+	@Override
+	public void unbind(final TrackingLog trackingLog) {
+		SelectChoices statusChoices;
+		Dataset dataset;
+
+		statusChoices = SelectChoices.from(TrackingLogStatus.class, trackingLog.getStatus());
+
+		dataset = super.unbindObject(trackingLog, "lastUpdateMoment", "step", "resolutionPercentage", "status", "resolution", "publish");
+		dataset.put("claimId", trackingLog.getClaim().getId());
+		dataset.put("status", statusChoices);
+
+		super.getResponse().addData(dataset);
+	}
 
 }

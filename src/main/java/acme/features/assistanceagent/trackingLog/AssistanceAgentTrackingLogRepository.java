5b822856dfc8474e5c4138577b243bbcbd628cd9
alereyper <alereyper@alum.us.es>
2025-04-02 19:58:36 +0200

    Tracking logs controller, repository and list, all empty, added

---
+++ src/main/java/acme/features/assistanceagent/trackingLog/AssistanceAgentTrackingLogRepository.java
@@ -0,0 +1,11 @@
+
+package acme.features.assistanceagent.trackingLog;
+
+import org.springframework.stereotype.Repository;
+
+import acme.client.repositories.AbstractRepository;
+
+@Repository
+public interface AssistanceAgentTrackingLogRepository extends AbstractRepository {
+
+}

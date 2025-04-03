40dadd4c6513e72b084c2a45468fd881b6a225bb
alereyper <alereyper@alum.us.es>
2025-04-03 17:15:05 +0200

    Tracking logs update service added

--- src/main/java/acme/features/assistanceagent/trackingLog/AssistanceAgentTrackingLogUpdateService.java
+++ src/main/java/acme/features/assistanceagent/trackingLog/AssistanceAgentTrackingLogUpdateService.java
@@ -3,9 +3,12 @@ package acme.features.assistanceagent.trackingLog;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import acme.client.components.models.Dataset;
+import acme.client.components.views.SelectChoices;
 import acme.client.services.AbstractGuiService;
 import acme.client.services.GuiService;
 import acme.entities.trackingLogs.TrackingLog;
+import acme.entities.trackingLogs.TrackingLogStatus;
 import acme.realms.assistanceagent.AssistanceAgent;
 
 @GuiService
@@ -17,5 +20,63 @@ public class AssistanceAgentTrackingLogUpdateService extends AbstractGuiService<
 	private AssistanceAgentTrackingLogRepository repository;
 
 	// AbstractGuiService interface -------------------------------------------
+
+
+	@Override
+	public void authorise() {
+		TrackingLog trackingLog;
+		int trackingLogId;
+		int agentId;
+		boolean status;
+
+		trackingLogId = super.getRequest().getData("id", int.class);
+		trackingLog = this.repository.findTrackingLogById(trackingLogId);
+		agentId = super.getRequest().getPrincipal().getActiveRealm().getId();
+		status = trackingLog.getClaim() != null && trackingLog != null && !trackingLog.isPublish() && trackingLog.getClaim().getAssistanceAgent().getId() == agentId;
+
+		super.getResponse().setAuthorised(status);
+	}
+
+	@Override
+	public void load() {
+		TrackingLog trackingLog;
+		int trackingLogId;
+
+		trackingLogId = super.getRequest().getData("id", int.class);
+		trackingLog = this.repository.findTrackingLogById(trackingLogId);
+
+		super.getBuffer().addData(trackingLog);
+	}
+
+	@Override
+	public void bind(final TrackingLog trackingLog) {
+		super.bindObject(trackingLog, "lastUpdateMoment", "step", "resolutionPercentage", "status", "resolution");
+	}
+
+	@Override
+	public void validate(final TrackingLog trackingLog) {
+		;
+	}
+
+	@Override
+	public void perform(final TrackingLog trackingLog) {
+		assert trackingLog != null;
+
+		this.repository.save(trackingLog);
+	}
+
+	@Override
+	public void unbind(final TrackingLog trackingLog) {
+		SelectChoices statusChoices;
+		Dataset dataset;
+
+		statusChoices = SelectChoices.from(TrackingLogStatus.class, trackingLog.getStatus());
+
+		dataset = super.unbindObject(trackingLog, "lastUpdateMoment", "step", "resolutionPercentage", "status", "resolution", "publish");
+		dataset.put("status", statusChoices);
+		dataset.put("claimId", trackingLog.getClaim().getId());
+
+		super.getResponse().addData(dataset);
+	}
 
 }
